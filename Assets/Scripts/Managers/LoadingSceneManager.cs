77873f7ab02a7095b04ed2d9ee2feb0831aa46c5
Jew <dima.com655@gmail.com>
2023-02-28 21:13:56 +0300

    Add Lobby Scene And UI

--- Assets/Scripts/Managers/LoadingSceneManager.cs
+++ Assets/Scripts/Managers/LoadingSceneManager.cs
@@ -6,15 +6,46 @@ using UnityEngine.SceneManagement;
 
 public enum SceneName : byte
 {
+    NextScene,
     Menu,
+    LobbySelection,
     CharacterSelection,
     Gameplay
 }
 
 public class LoadingSceneManager : SingletonPersistent<LoadingSceneManager>
 {
-    public void loadScene(SceneName sceneName)
+    public void LoadScene(SceneName sceneName)
     {
+        switch (sceneName)
+        {
+            case SceneName.NextScene:
+                int currentIndex = SceneManager.GetActiveScene().buildIndex;
+                Load(currentIndex);
+                break;
 
+            case SceneName.Menu:
+                Load(0);
+                break;
+
+            case SceneName.LobbySelection:
+                Load(1);
+                break;
+
+            case SceneName.CharacterSelection:
+                Load(2);
+                break;
+        }
+    }
+
+    private void Load(int index)
+    {
+        if (SceneManager.GetSceneAt(index).IsValid())
+        {
+            Debug.Log($"Scene At: {index} | " % Colorize.Yellow % FontFormat.Bold + $"Not Found" % Colorize.Red % FontFormat.Bold);
+            return;
+        }
+
+        SceneManager.LoadScene(index);
     }
 }
--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -4,33 +4,33 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private Button hostButton;
-    [SerializeField] private Button joinButton;
+    [SerializeField] private Button lobbyButton;
     [SerializeField] private Button quitButton;
 
     private void Awake()
     {
-        if (hostButton != null)
-            hostButton.onClick.AddListener(() => OnClickHost());
-        else Debug.Log("host Button " % Colorize.Yellow % FontFormat.Bold + "| Is Null |" % Colorize.Red % FontFormat.Bold);
-       
-        if (joinButton != null)
-            joinButton.onClick.AddListener(() => OnClickJoin());
-        else Debug.Log("Joined Button " % Colorize.Yellow % FontFormat.Bold + "| Is Null |" % Colorize.Red % FontFormat.Bold);
-      
+        if (lobbyButton != null)
+            lobbyButton.onClick.AddListener(() => OnClickLobby());
+        else Debug.Log("Lobby Button " % Colorize.Yellow % FontFormat.Bold + "| Is Null |" % Colorize.Red % FontFormat.Bold);
+             
         if (quitButton != null)
             quitButton.onClick.AddListener(() => Quit());
         else Debug.Log("Quit Button " % Colorize.Yellow % FontFormat.Bold + "| Is Null |" % Colorize.Red % FontFormat.Bold);
     }
 
+    private void OnClickLobby()
+    {
+        LoadingSceneManager.Instance.LoadScene(SceneName.LobbySelection);
+    }
+
     private void OnClickHost()
     {
-        LobbyManager.Instance.CreateLobby();
+        //LobbyManager.Instance.CreateLobby();
     }
 
     private void OnClickJoin()
     {
-        NetworkManager.Singleton.StartClient();
+        //NetworkManager.Singleton.StartClient();
     }
 
     private void Quit()
